9b10071e2560103ac60bbbee2ee0814a6cf67a3b
ibrahimozekici <ozekici07@gmail.com>
2021-10-06 16:09:34 +0300

    commit

--- go/as/external/api/internal.pb.gw.go
+++ go/as/external/api/internal.pb.gw.go
@@ -389,6 +389,39 @@ func local_request_InternalService_CreateLicenceOrder_0(ctx context.Context, mar
 
 }
 
+var (
+	filter_InternalService_GetLicenceOrder_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_InternalService_GetLicenceOrder_0(ctx context.Context, marshaler runtime.Marshaler, client InternalServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CreateLicenceOrderRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_InternalService_GetLicenceOrder_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetLicenceOrder(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_InternalService_GetLicenceOrder_0(ctx context.Context, marshaler runtime.Marshaler, server InternalServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CreateLicenceOrderRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_InternalService_GetLicenceOrder_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetLicenceOrder(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterInternalServiceHandlerServer registers the http handlers for service InternalService to "mux".
 // UnaryRPC     :call InternalServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -611,6 +644,26 @@ func RegisterInternalServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		}
 
 		forward_InternalService_CreateLicenceOrder_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_InternalService_GetLicenceOrder_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_InternalService_GetLicenceOrder_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_InternalService_GetLicenceOrder_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -875,6 +928,26 @@ func RegisterInternalServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
+	mux.Handle("GET", pattern_InternalService_GetLicenceOrder_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_InternalService_GetLicenceOrder_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_InternalService_GetLicenceOrder_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -900,6 +973,8 @@ var (
 	pattern_InternalService_GetGatewaysSummary_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "internal", "gateways", "summary"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_InternalService_CreateLicenceOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "internal", "order"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_InternalService_GetLicenceOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "internal", "order"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -924,4 +999,6 @@ var (
 	forward_InternalService_GetGatewaysSummary_0 = runtime.ForwardResponseMessage
 
 	forward_InternalService_CreateLicenceOrder_0 = runtime.ForwardResponseMessage
+
+	forward_InternalService_GetLicenceOrder_0 = runtime.ForwardResponseMessage
 )
