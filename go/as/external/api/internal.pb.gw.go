62bcb1f06c97d43cdde3fb7864b8cc15d52519c5
ibrahimozekici <ozekici07@gmail.com>
2021-10-06 17:28:07 +0300

    c

--- go/as/external/api/internal.pb.gw.go
+++ go/as/external/api/internal.pb.gw.go
@@ -389,19 +389,26 @@ func local_request_InternalService_CreateLicenceOrder_0(ctx context.Context, mar
 
 }
 
-var (
-	filter_InternalService_GetLicenceOrder_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_InternalService_GetLicenceOrder_0(ctx context.Context, marshaler runtime.Marshaler, client InternalServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetLicenceOrderRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["order_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "order_id")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_InternalService_GetLicenceOrder_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+
+	protoReq.OrderId, err = runtime.Uint32(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "order_id", err)
 	}
 
 	msg, err := client.GetLicenceOrder(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -413,8 +420,22 @@ func local_request_InternalService_GetLicenceOrder_0(ctx context.Context, marsha
 	var protoReq GetLicenceOrderRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_InternalService_GetLicenceOrder_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["order_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "order_id")
+	}
+
+	protoReq.OrderId, err = runtime.Uint32(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "order_id", err)
 	}
 
 	msg, err := server.GetLicenceOrder(ctx, &protoReq)
@@ -974,7 +995,7 @@ var (
 
 	pattern_InternalService_CreateLicenceOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "internal", "order"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_InternalService_GetLicenceOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "internal", "order"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_InternalService_GetLicenceOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "internal", "order", "order_id"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
