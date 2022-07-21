8228f53ea533878dc775e8534c8a0c70d205d184
ibrahimozekici <ozekici07@gmail.com>
2022-07-21 08:24:16 +0300

    c

--- go/as/external/api/alarm.pb.gw.go
+++ go/as/external/api/alarm.pb.gw.go
@@ -325,34 +325,34 @@ func local_request_AlarmService_DeleteMultiple_0(ctx context.Context, marshaler 
 }
 
 var (
-	filter_AlarmService_GetMultiple_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_AlarmService_GetOrganizationAlarms_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
-func request_AlarmService_GetMultiple_0(ctx context.Context, marshaler runtime.Marshaler, client AlarmServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_AlarmService_GetOrganizationAlarms_0(ctx context.Context, marshaler runtime.Marshaler, client AlarmServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListMultipleAlarmRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AlarmService_GetMultiple_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AlarmService_GetOrganizationAlarms_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.GetMultiple(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.GetOrganizationAlarms(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_AlarmService_GetMultiple_0(ctx context.Context, marshaler runtime.Marshaler, server AlarmServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_AlarmService_GetOrganizationAlarms_0(ctx context.Context, marshaler runtime.Marshaler, server AlarmServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListMultipleAlarmRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_AlarmService_GetMultiple_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_AlarmService_GetOrganizationAlarms_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.GetMultiple(ctx, &protoReq)
+	msg, err := server.GetOrganizationAlarms(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -502,7 +502,7 @@ func RegisterAlarmServiceHandlerServer(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
-	mux.Handle("GET", pattern_AlarmService_GetMultiple_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_AlarmService_GetOrganizationAlarms_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -511,14 +511,14 @@ func RegisterAlarmServiceHandlerServer(ctx context.Context, mux *runtime.ServeMu
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_AlarmService_GetMultiple_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_AlarmService_GetOrganizationAlarms_0(rctx, inboundMarshaler, server, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_AlarmService_GetMultiple_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_AlarmService_GetOrganizationAlarms_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -703,7 +703,7 @@ func RegisterAlarmServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
-	mux.Handle("GET", pattern_AlarmService_GetMultiple_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_AlarmService_GetOrganizationAlarms_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -712,14 +712,14 @@ func RegisterAlarmServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_AlarmService_GetMultiple_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_AlarmService_GetOrganizationAlarms_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_AlarmService_GetMultiple_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_AlarmService_GetOrganizationAlarms_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -741,7 +741,7 @@ var (
 
 	pattern_AlarmService_DeleteMultiple_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "alarm", "deleteMultiple"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_AlarmService_GetMultiple_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "alarm", "listMultiple"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_AlarmService_GetOrganizationAlarms_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "alarm", "organizationAlarms"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -759,5 +759,5 @@ var (
 
 	forward_AlarmService_DeleteMultiple_0 = runtime.ForwardResponseMessage
 
-	forward_AlarmService_GetMultiple_0 = runtime.ForwardResponseMessage
+	forward_AlarmService_GetOrganizationAlarms_0 = runtime.ForwardResponseMessage
 )
