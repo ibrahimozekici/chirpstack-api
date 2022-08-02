73012de875f44008ff41941d6b99bd641f9c36d0
ulas <yurttasutkan@gmail.com>
2022-08-02 10:53:40 +0300

    c

--- go/als/als.pb.go
+++ go/als/als.pb.go
@@ -25,6 +25,7 @@ import (
 )
 
 const (
+	//a
 	// Verify that this generated code is sufficiently up-to-date.
 	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
 	// Verify that runtime/protoimpl is sufficiently up-to-date.
